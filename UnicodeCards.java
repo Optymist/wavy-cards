6ff37d07b45aed6afe25580c348da115eab15ddd
Optymist <kimlynsmart@gmail.com>
2024-07-22 14:22:25 +0200

    Added all unicode characters for playing cards that can eventually be used with a GUI.

---
+++ UnicodeCards.java
@@ -0,0 +1,63 @@
+public class UnicodeCards {
+    // Spades
+    private static String back = "\uD83C\uDCA0";
+    private static String aceSpades = "\uD83C\uDCA1";
+    private static String twoSpades = "\uD83C\uDCA2";
+    private static String threeSpades = "\uD83C\uDCA3";
+    private static String fourSpades = "\uD83C\uDCA4";
+    private static String fiveSpades = "\uD83C\uDCA5";
+    private static String sixSpades = "\uD83C\uDCA6";
+    private static String sevenSpades = "\uD83C\uDCA7";
+    private static String eightSpades = "\uD83C\uDCA8";
+    private static String nineSpades = "\uD83C\uDCA9";
+    private static String tenSpades = "\uD83C\uDCAA";
+    private static String jackSpades = "\uD83C\uDCAB";
+    private static String queenSpades = "\uD83C\uDCAC";
+    private static String kingSpades = "\uD83C\uDCAD";
+
+    // Hearts
+    private static String aceHearts = "\uD83C\uDCA8";
+    private static String twoHearts = "\uD83C\uDCA9";
+    private static String threeHearts = "\uD83C\uDCAA";
+    private static String fourHearts = "\uD83C\uDCAB";
+    private static String fiveHearts = "\uD83C\uDCAC";
+    private static String sixHearts = "\uD83C\uDCAD";
+    private static String sevenHearts = "\uD83C\uDCAE";
+    private static String eightHearts = "\uD83C\uDCAF";
+    private static String nineHearts = "\uD83C\uDCB0";
+    private static String tenHearts = "\uD83C\uDCB1";
+    private static String jackHearts = "\uD83C\uDCB2";
+    private static String queenHearts = "\uD83C\uDCB3";
+    private static String kingHearts = "\uD83C\uDCB4";
+
+    // Diamonds
+    private static String aceDiamonds = "\uD83C\uDCA7";
+    private static String twoDiamonds = "\uD83C\uDCA8";
+    private static String threeDiamonds = "\uD83C\uDCA9";
+    private static String fourDiamonds = "\uD83C\uDCAA";
+    private static String fiveDiamonds = "\uD83C\uDCAB";
+    private static String sixDiamonds = "\uD83C\uDCAC";
+    private static String sevenDiamonds = "\uD83C\uDCAD";
+    private static String eightDiamonds = "\uD83C\uDCAE";
+    private static String nineDiamonds = "\uD83C\uDCAF";
+    private static String tenDiamonds = "\uD83C\uDCB0";
+    private static String jackDiamonds = "\uD83C\uDCB1";
+    private static String queenDiamonds = "\uD83C\uDCB2";
+    private static String kingDiamonds = "\uD83C\uDCB3";
+
+    // Clubs
+    private static String aceClubs = "\uD83C\uDCA3";
+    private static String twoClubs = "\uD83C\uDCA4";
+    private static String threeClubs = "\uD83C\uDCA5";
+    private static String fourClubs = "\uD83C\uDCA6";
+    private static String fiveClubs = "\uD83C\uDCA7";
+    private static String sixClubs = "\uD83C\uDCA8";
+    private static String sevenClubs = "\uD83C\uDCA9";
+    private static String eightClubs = "\uD83C\uDCAA";
+    private static String nineClubs = "\uD83C\uDCAB";
+    private static String tenClubs = "\uD83C\uDCAC";
+    private static String jackClubs = "\uD83C\uDCAD";
+    private static String queenClubs = "\uD83C\uDCAE";
+    private static String kingClubs = "\uD83C\uDCAF";
+
+}
