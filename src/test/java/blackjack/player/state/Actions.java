1ebcc815582a9a55e37adb4bb02cf8834c24906e
scarr023 <scarr023@student.wethinkcode.co.za>
2024-09-13 11:26:35 +0200

    test name

--- src/test/java/blackjack/player/state/Actions.java
+++ src/test/java/blackjack/player/state/Actions.java
@@ -7,6 +7,8 @@ import blackjack.actions.DoubleAction;
 import blackjack.actions.HitAction;
 import blackjack.actions.StandAction;
 import blackjack.actions.SurrenderAction;
+import blackjack.deck.Card;
+import blackjack.deck.Deck;
 import blackjack.player.Hand;
 
 import org.junit.jupiter.api.Test;
@@ -37,7 +39,6 @@ public class Actions {
         assertEquals(0, actions.size());
     }
 
-
     @Test
     public void testSurrender() {
         playerState state = new Stand();
@@ -45,14 +46,12 @@ public class Actions {
         assertEquals(0, actions.size());
     }
 
-
     @Test
     public void testNormalNoSplit() {
         playerState state = new Normal();
         List<BlackJackAction> actions = state.getActions(new Hand());
         ArrayList<BlackJackAction> expectedActions = new ArrayList<>(List.of(
-            new DoubleAction(), new HitAction(), new StandAction(), new SurrenderAction()
-        ));
+                new DoubleAction(), new HitAction(), new StandAction(), new SurrenderAction()));
         assertEquals(4, actions.size());
         aseertContainsActions(expectedActions, actions);
     }
@@ -61,6 +60,8 @@ public class Actions {
     public void testNormalWithSplit() {
         playerState state = new Normal();
         Hand hand = new Hand();
+        Card card0 = new Card(Deck.SUITS[0], Deck.RANKS[0]);
+        Card card1 = new Card(Deck.SUITS[1], Deck.RANKS[1]);
 
     }
 
