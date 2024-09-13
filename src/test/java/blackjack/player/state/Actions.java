f855aa0d38f9578bed4c3f1b9f1b6124fb4e23b5
scarr023 <scarr023@student.wethinkcode.co.za>
2024-09-13 09:44:13 +0200

    actions tests 1

--- src/test/java/blackjack/player/state/Actions.java
+++ src/test/java/blackjack/player/state/Actions.java
@@ -3,7 +3,12 @@ package blackjack.player.state;
 import static org.junit.jupiter.api.Assertions.*;
 
 import blackjack.actions.BlackJackAction;
+import blackjack.actions.DoubleAction;
+import blackjack.actions.HitAction;
+import blackjack.actions.StandAction;
+import blackjack.actions.SurrenderAction;
 import blackjack.player.Hand;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -25,6 +30,54 @@ public class Actions {
         assertEquals(0, actions.size());
     }
 
+    @Test
+    public void testStand() {
+        playerState state = new Stand();
+        List<BlackJackAction> actions = state.getActions(new Hand());
+        assertEquals(0, actions.size());
+    }
 
+
+    @Test
+    public void testSurrender() {
+        playerState state = new Stand();
+        List<BlackJackAction> actions = state.getActions(new Hand());
+        assertEquals(0, actions.size());
+    }
+
+
+    @Test
+    public void testNormalNoSplit() {
+        playerState state = new Normal();
+        List<BlackJackAction> actions = state.getActions(new Hand());
+        ArrayList<BlackJackAction> expectedActions = new ArrayList<BlackJackAction>(List.of(
+            new DoubleAction(), new HitAction(), new StandAction(), new SurrenderAction()
+        ));
+        assertEquals(4, actions.size());
+        aseertContainsActions(expectedActions, actions);
+    }
+
+    @Test
+    public void testNormalWithSplit() {
+        playerState state = new Normal();
+        Hand hand = new Hand();
+
+    }
+
+    public static void aseertContainsActions(List<BlackJackAction> expected, List<BlackJackAction> actual) {
+        for (BlackJackAction blackJackAction : expected) {
+            assertActionExists(blackJackAction, actual);
+        }
+    }
+
+    public static void assertActionExists(BlackJackAction expected, List<BlackJackAction> actual) {
+        for (BlackJackAction blackJackAction : actual) {
+            if (blackJackAction.getClass() == expected.getClass()) {
+                return;
+            }
+        }
+
+        throw new AssertionError(String.format("%s not in actions", expected));
+    }
 
 }
