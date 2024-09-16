51686c91fd704bec6c44862a0ce89c9fdd662f80
Optymist <kimlynsmart@gmail.com>
2024-09-16 10:34:09 +0200

    Deck size test for initialization of the deck when the game starts.

--- src/test/java/blackjack/PlayTest.java
+++ src/test/java/blackjack/PlayTest.java
@@ -1,14 +1,39 @@
 package blackjack;
 
-import java.util.List;
+import blackjack.deck.Deck;
 
+import blackjack.helperClasses.mockedPlayerManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 /**
  * PlayTest
  */
 public class PlayTest {
+    private PlayerManager mockPlayerManager;
 
-// todo :)
+    @BeforeEach
+    public void setUp() {
+        mockedPlayerManager.setUp();
+        this.mockPlayerManager = mockedPlayerManager.mockPlayerManager;
+    }
+
+    @Test
+    public void testStartGameWithCorrectAmountOfDecks() {
+        Play game = new Play(2);
+        Deck gameDeck = game.getDeck();
+        assertEquals(52, gameDeck.getPlayDeck().size());
+
+        Play game2 = new Play(5);
+        Deck game2Deck = game2.getDeck();
+        assertEquals(104, game2Deck.getPlayDeck().size());
+    }
+
+
+
+
+
 }
