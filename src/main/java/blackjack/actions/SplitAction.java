2c443464ed884e958a5ab5e6a525890da4dfea27
Optymist <kimlynsmart@gmail.com>
2024-09-27 11:19:14 +0200

    Split docstrings.

--- src/main/java/blackjack/actions/SplitAction.java
+++ src/main/java/blackjack/actions/SplitAction.java
@@ -5,11 +5,20 @@ import blackjack.player.Hand;
 import blackjack.player.Player;
 import blackjack.protocol.GenerateJson;
 
+/**
+ * SplitAction class
+ */
 public class SplitAction extends BlackJackAction {
     public SplitAction() {
         super("split");
     }
 
+    /**
+     * Calls the method to split the player's hand and sends output to the client.
+     * @param playingHand --> hand being played
+     * @param player --> player who's choosing the action
+     * @param game --> game being played
+     */
     @Override
     public void execute(Hand playingHand, Player player, Play game) {
         Hand splitPlayerHand = player.splitHand(game);
@@ -17,8 +26,6 @@ public class SplitAction extends BlackJackAction {
         player.getPlayerManager().sendMessage(GenerateJson.generateGeneralMessage(player.getCardsInHand().toString()));
         player.getPlayerManager().sendMessage(GenerateJson.generateGeneralMessage(splitPlayerHand.toString()));
     }
-
-
 
     @Override
     public String toString() {
